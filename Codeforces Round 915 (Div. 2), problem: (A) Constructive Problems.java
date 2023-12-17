f8e0596e827295798e278d6f09723d1ca98d444a
Arihant kumar jain <125581360+arihant1805@users.noreply.github.com>
2023-12-17 17:49:01 +0530

    Create Codeforces Round 915 (Div. 2), problem: (A) Constructive Problems.java

---
+++ Codeforces Round 915 (Div. 2), problem: (A) Constructive Problems.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Solution{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T=sc.nextInt();
+        while(T-->0){
+            int m=sc.nextInt();
+            int n=sc.nextInt();
+            System.out.println(Math.max(m,n));
+        }
+    }
+}
